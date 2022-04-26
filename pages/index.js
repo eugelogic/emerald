121148b3ef422a4159fbab6ffe36a95d7f3bfded
Eugene Molari <eugelogic@gmail.com>
2022-04-26 14:21:07 +0100

    Migrate code for blog index to dedicated file

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import PostItem from '../components/PostItem'
-import { sortByDate } from '../utils'
-
-export const getStaticProps = async () => {
-    const files = fs.readdirSync(path.join('posts'))
-
-    const posts = files.map((filename) => {
-        const slug = filename.replace('.md', '')
-        const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf8')
-        const { data: frontmatter } = matter(markdownWithMeta)
-
-        return {
-            slug,
-            frontmatter,
-        }
-    })
-
-    return {
-        props: {
-            posts: posts.sort(sortByDate),
-        },
-    }
-}
-
-const Home = ({ posts }) => {
+const Home = () => {
     return (
         <div>
             <h1>Eugene Molari Dev</h1>
-            <ul>{posts?.length > 0 && posts.map((post, index) => <PostItem key={index} post={post} />)}</ul>
         </div>
     )
 }
