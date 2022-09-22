a9d93846e64fbb3d5841b98b347a6703b3ee0e93
Eugene Molari <eugelogic@gmail.com>
2022-09-22 14:16:06 +0100

    Add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Quotes from '../public/quotes.json'
 
@@ -14,7 +15,15 @@ const Home = () => {
                 <div className={`${styles.box} ${styles.lt}`}>
                     <div className={`${styles.box} ${styles.ltt}`}>
                         <div className={`${styles.box} ${styles.lttl}`}>
-                            <div className={`${styles.box} ${styles.mobile}`}>PIC</div>
+                            <div className={`${styles.box} ${styles.mobile}`}>
+                                <Image
+                                    src="/eugene-molari.jpg"
+                                    alt="Eugene Molari"
+                                    width={542}
+                                    height={542}
+                                    layout="fill"
+                                />
+                            </div>
                             <div className={styles.box}>M</div>
                             <div className={styles.box}>S</div>
                             <div className={styles.box}>I</div>
@@ -23,7 +32,15 @@ const Home = () => {
                         <div className={`${styles.box} ${styles.lttr}`}>Eugene</div>
                     </div>
                     <div className={`${styles.box} ${styles.ltb}`}>
-                        <div className={`${styles.box} ${styles.ltbl}`}>PIC</div>
+                        <div className={`${styles.box} ${styles.ltbl}`}>
+                            <Image
+                                src="/eugene-molari.jpg"
+                                alt="Eugene Molari"
+                                width={542}
+                                height={542}
+                                layout="fill"
+                            />
+                        </div>
                         <div className={`${styles.box} ${styles.ltbr}`}>
                             <p>“{randomQuote.text}”</p>
                             <p>• {randomQuote.author}</p>
