aea340911c07e213f4957e0aa6cfea337bcd3d91
Eugene Molari <eugelogic@gmail.com>
2022-09-23 22:53:06 +0100

    Make main nav menu more semantic

--- pages/menu.js
+++ pages/menu.js
@@ -6,15 +6,7 @@ const Menu = () => {
     const router = useRouter()
 
     return (
-        <div className={styles.navHack}>
-            <button className={styles.backButton} type="button" onClick={() => router.back()}>
-                <svg width="46" height="46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z"
-                        fill="#fff"
-                    />
-                </svg>
-            </button>
+        <nav className={styles.navHack}>
             <ul>
                 <li>
                     <Link href="/">
@@ -52,7 +44,15 @@ const Menu = () => {
                     </Link>
                 </li>
             </ul>
-        </div>
+            <button className={styles.backButton} type="button" onClick={() => router.back()}>
+                <svg width="46" height="46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z"
+                        fill="#fff"
+                    />
+                </svg>
+            </button>
+        </nav>
     )
 }
 
