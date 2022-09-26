9eaf6a0b9d9442a526286e4f58d79ea5a375df43
Eugene Molari <eugelogic@gmail.com>
2022-09-26 16:45:55 +0100

    Add Layout component

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+    const router = useRouter()
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="./favicon.ico" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <title>{router.pathname === '/' ? 'Eugene Molari Digital' : 'Eugene Molari Digital'}</title>
+                <meta name="description" content="Product Designer" />
+            </Head>
+            <div>
+                <Header />
+                <main>{children}</main>
+                <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Layout
--- pages/blog.js
+++ pages/blog.js
@@ -2,9 +2,9 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import PostItem from '../components/PostItem'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Layout from '../components/Layout'
 import { sortByDate } from '../utils'
+import styles from '../styles/Blog.module.css'
 
 export const getStaticProps = async () => {
     const files = fs.readdirSync(path.join('posts'))
@@ -29,12 +29,19 @@ export const getStaticProps = async () => {
 
 const Blog = ({ posts }) => {
     return (
-        <div>
-            <Header />
-            <h1>Blog</h1>
-            <ul>{posts?.length > 0 && posts.map((post, index) => <PostItem key={index} post={post} />)}</ul>
-            <Footer />
-        </div>
+        <Layout>
+            <div className={styles.container}>
+                <h1>Blog</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <ul>{posts?.length > 0 && posts.map((post, index) => <PostItem key={index} post={post} />)}</ul>
+            </div>
+        </Layout>
     )
 }
 
