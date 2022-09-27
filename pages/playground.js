6dc273c1d4ae14ac004138207e4f64283a126e4c
Eugene Molari <eugelogic@gmail.com>
2022-09-27 16:22:37 +0100

    Add first actual playground entry

--- pages/playground.js
+++ pages/playground.js
@@ -8,13 +8,17 @@ const Playground = () => {
             <div className={styles.container}>
                 <h1>Playground</h1>
                 <p>
-                    Web design and development often make me feel like a kid in a candy store, there is so much going
-                    on. Publishing something on the web is also fairly easy compared to the printed press for instance.
+                    Web design and development often make me feel like a kid in a candy shop, there is so much going on.
+                    Publishing something on the web is also fairly easy compared to the printed press for instance.
                 </p>
                 <p>
                     Designing websites can be restrictive though hence this playground where I like to let myself loose
                     and take licences. I like to challenge myself with extravagant ideas that came to life with the aid
-                    of techniques like SCAMPER.
+                    of techniques like{' '}
+                    <a href="https://en.wikipedia.org/wiki/SCAMPER" target="_blank" rel="noopener noreferrer">
+                        SCAMPER
+                    </a>
+                    .
                 </p>
                 <p>I get inspired by magazines, architecture, music, fashion, art and industrial design.</p>
                 <ul className={styles.playgroundList}>
@@ -30,11 +34,16 @@ const Playground = () => {
                             <span>#DESIGN #CODE</span>
                         </div>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            This is one of my favourites. I thought it would be easier to put together 15 different font
+                            families; far from it. I got the idea from the{' '}
+                            <a
+                                href="https://profilebooks.com/work/just-my-type/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                “Just My Type” book
+                            </a>
+                            . If you curious, click each letter to learn what font I used.
                         </p>
                     </PlayEntry>
                     <PlayEntry
---
+++ pages/playground/whats-your-type/index.js
@@ -0,0 +1,189 @@
+/* eslint-disable */
+import Head from 'next/head'
+import styles from './whats-your-type.module.css'
+
+const WhatsYourType = () => {
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Tangerine&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Tourney:wght@100&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Rye&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Megrim&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Palette+Mosaic&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Baumans&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Emilys+Candy&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Original+Surfer&display=swap" rel="stylesheet" />
+            </Head>
+            <div className={styles.container}>
+                <h1 className={styles.text}>
+                    <span className={styles.wUp}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Abril+Fatface"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            W
+                        </a>
+                    </span>
+                    <span className={styles.h}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Tangerine"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            h
+                        </a>
+                    </span>
+                    <span className={styles.a}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Gochi+Hand"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            a
+                        </a>
+                    </span>
+                    <span className={styles.t}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Tourney"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            t
+                        </a>
+                    </span>
+                    <span className={styles.apo}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Rye"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            '
+                        </a>
+                    </span>
+                    <span className={styles.s}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Courier+Prime"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            s
+                        </a>
+                    </span>
+                </h1>
+                <h1 className={styles.text}>
+                    <span className={styles.yUp}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Megrim"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Y
+                        </a>
+                    </span>
+                    <span className={styles.o}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Press+Start+2P"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            o
+                        </a>
+                    </span>
+                    <span className={styles.u}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Orbitron"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            u
+                        </a>
+                    </span>
+                    <span className={styles.r}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Palette+Mosaic"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            r
+                        </a>
+                    </span>
+                </h1>
+                <h1 className={styles.text}>
+                    <span className={styles.tUp}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Bungee+Shade"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            T
+                        </a>
+                    </span>
+                    <span className={styles.y}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Indie+Flower"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            y
+                        </a>
+                    </span>
+                    <span className={styles.p}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Baumans"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            p
+                        </a>
+                    </span>
+                    <span className={styles.e}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Emilys+Candy"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            e
+                        </a>
+                    </span>
+                    <span className={styles.qm}>
+                        <a
+                            className={styles.link}
+                            href="https://fonts.google.com/specimen/Original+Surfer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            ?
+                        </a>
+                    </span>
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default WhatsYourType
