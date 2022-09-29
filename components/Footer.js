4bbb1c93857d9929d5ce4f0646a593ff59cca281
Eugene Molari <eugelogic@gmail.com>
2022-09-29 13:48:09 +0100

    Add content to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,20 @@ import styles from '../styles/Footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
+            <p>
+                Site built with{' '}
+                <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
+                    NextJS
+                </a>{' '}
+                and hosted on{' '}
+                <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">
+                    Vercel
+                </a>{' '}
+                for free :-)
+            </p>
             <Link href="/">
                 <a>
-                    <span>&copy; {new Date().getFullYear()} Eugene Molari Digital</span>
+                    <span>&copy; 2007-{new Date().getFullYear()} Eugene Molari Digital</span>
                 </a>
             </Link>
         </footer>
