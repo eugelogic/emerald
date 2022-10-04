e38404a2e3c267be59b91224a1e7a9f96d9768f7
Eugene Molari <eugelogic@gmail.com>
2022-10-04 21:09:06 +0100

    Add Head tags to the Main Menu page

--- pages/menu.js
+++ pages/menu.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import styles from '../styles/Menu.module.css'
@@ -6,53 +7,61 @@ const Menu = () => {
     const router = useRouter()
 
     return (
-        <nav className={styles.navHack}>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <a>Home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <a>About</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/work">
-                        <a>Work</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/reviews">
-                        <a>Reviews</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/playground">
-                        <a>Playground</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/people">
-                        <a>People</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/links">
-                        <a>Links</a>
-                    </Link>
-                </li>
-            </ul>
-            <button className={styles.backButton} type="button" onClick={() => router.back()}>
-                <svg width="46" height="46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z"
-                        fill="#fff"
-                    />
-                </svg>
-            </button>
-        </nav>
+        <>
+            <Head>
+                <title>Main Menu | Eugene Molari Digital </title>
+                <meta name="description" content="Product Designer" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="icon" href="./favicon.ico" />
+            </Head>
+            <nav className={styles.navHack}>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/work">
+                            <a>Work</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/reviews">
+                            <a>Reviews</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/playground">
+                            <a>Playground</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/people">
+                            <a>People</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/links">
+                            <a>Links</a>
+                        </Link>
+                    </li>
+                </ul>
+                <button className={styles.backButton} type="button" onClick={() => router.back()}>
+                    <svg width="46" height="46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z"
+                            fill="#fff"
+                        />
+                    </svg>
+                </button>
+            </nav>
+        </>
     )
 }
 
