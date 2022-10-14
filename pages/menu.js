0d97c58d5a6152f0cbc3ac049f6c3c4bcf6d2f2a
Eugene Molari <eugelogic@gmail.com>
2022-10-14 09:17:01 +0100

    Add hover over effects to back button (X) on the main nav menu

--- pages/menu.js
+++ pages/menu.js
@@ -61,10 +61,7 @@ const Menu = () => {
                 </ul>
                 <button className={styles.backButton} type="button" onClick={() => router.back()}>
                     <svg width="46" height="46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z"
-                            fill="#fff"
-                        />
+                        <path d="M41.42 45.514 23.015 27.08 4.609 45.514.5 41.405 18.934 23 .5 4.595 4.61.485 23.014 18.92 41.42.515l4.08 4.08L27.095 23 45.5 41.405l-4.08 4.11Z" />
                     </svg>
                 </button>
             </nav>
