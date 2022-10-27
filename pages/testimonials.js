b9286b443c644f94f7092a5af62b02e6017e5e04
Eugene Molari <eugelogic@gmail.com>
2022-10-27 15:15:05 +0100

    Draft page for testimonials

---
+++ pages/testimonials.js
@@ -0,0 +1,47 @@
+import Layout from '../components/Layout'
+
+const Testimonials = () => {
+    return (
+        <Layout title="Testimonials">
+            <div className="container">
+                <h1>Testimonials</h1>
+                <h2>Christopher Geary</h2>
+                <h3>Co-worker @ Indigo Tree Digital</h3>
+                <p>
+                    He and I worked together at IndigoTree, where his position was &apos;Web Developer&apos; and my
+                    position was &apos;Senior Web Developer&apos;. Eugene has great communication skills, a high
+                    attention to detail, and a wealth of experience with content management systems. Those combined with
+                    his understanding of web development, earnt him full ownership of projects, which was unheard of for
+                    a developer at IndigoTree. During the 3 years we worked together, Eugene always demonstrated he was
+                    motivated and keen to learn. He was proactive about doing his own research first, but was equally
+                    not afraid to ask questions or gain a second opinion. This, coupled by his attention to detail,
+                    meant he would make notes and document his findings, which proved to be invaluable for the company,
+                    as those notes formed the basis of the companies online internal knoweldge base, which he promptly
+                    became responsible for. Eugene is an absolute delight to work with, he&apos;s a very likeable
+                    person, always has a positive attitude, and a great team player. I wouldn&apos;t hesitate to work
+                    with him again.
+                </p>
+                <h2>Tom Greenwood</h2>
+                <h3>Managing Director @ Wholegrain Digital</h3>
+                <p>
+                    Eugene worked with us for three and a half years and was instrumental to the growth of our agency.
+                    Not only was he a dependable Project Manager loved by our team and our clients, but also a proactive
+                    contributor to our wider business strategy and management. He has a genuine passion for WordPress
+                    and the web, and consistently goes above and beyond to support teammates and clients. In addition to
+                    his role as Project Manager, Eugene rose to the challenge of managing the agency on numerous
+                    occasions during my absence, and is one of the very few people that I would ever trust to do this.
+                    His loyalty and trust are beyond doubt. His departure will be a great loss to our team, but a great
+                    asset to any team that he joins in the future.
+                </p>
+                <h2>Kelly Wilson</h2>
+                <h3>Co-worker @ Indigo Tree Digital</h3>
+                <p>
+                    I&apos;d be more than happy to act as a reference for Eugene, no problem to give me name out and
+                    I&apos;ll be happy to tell them how wonderful Eugene is.
+                </p>
+            </div>
+        </Layout>
+    )
+}
+
+export default Testimonials
